81641861b865324acf8b7b05362289c3e0a6e72f
MiguelJordao <1201487@isep.ipp.pt>
2021-05-05 02:20:55 +0100

    General information about PPROG part (Classes implementations)

--- src/main/java/app/ui/console/DevTeamUI.java
+++ src/main/java/app/ui/console/DevTeamUI.java
@@ -14,10 +14,11 @@ public class DevTeamUI implements Runnable{
     {
         System.out.println("\n");
         System.out.printf("Development Team:\n");
-        System.out.printf("\t Student Name 1 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 2 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 3 - 120XXXX@isep.ipp.pt \n");
-        System.out.printf("\t Student Name 4 - 120XXXX@isep.ipp.pt \n");
+        System.out.printf("\t Marcin Basinski  - 1200300@isep.ipp.pt \n");
+        System.out.printf("\t Daniel Costa - 1200581@isep.ipp.pt \n");
+        System.out.printf("\t Lucas Silva - 1200759@isep.ipp.pt \n");
+        System.out.printf("\t Mariana Lages - 1200902@isep.ipp.pt \n");
+        System.out.printf("\t Miguel Jordão - 1201487@isep.ipp.pt \n");
         System.out.println("\n");
     }
 }
