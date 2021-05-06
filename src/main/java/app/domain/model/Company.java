4a66ed2225b697d0a2313e013ec98a8962db1f43
MiguelJordao <1201487@isep.ipp.pt>
2021-05-06 16:51:26 +0100

    Minor changes in the class "Company"

--- src/main/java/app/domain/model/Company.java
+++ src/main/java/app/domain/model/Company.java
@@ -4,14 +4,66 @@ import app.controller.TestTypeController;
 import auth.AuthFacade;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.List;
+
 /**
  *
  * @author Paulo Maio <pam@isep.ipp.pt>
+ * @author Miguel Jordão <1201477@isep.ipp.pt>
+ * @author Mariana Lages <@isep.ipp.pt>
+ * @author Lucas Silva <@isep.ipp.pt>
+ * @author Daniel Costa <@isep.ipp.pt>
+ * @author Marcin Basinki <@isep.ipp.pt>
  */
 public class Company {
 
     private String designation;
     private AuthFacade authFacade;
+
+    TestTypeStore testTypeStore = new TestTypeStore();
+
+    private static TestTypeStore testTypeList;
+
+    public static TestTypeStore TestType(){
+        return testTypeList = new TestTypeStore();
+    }
+
+    //public List<TestType> getTestType(){return testTypeStore.getTt();}
+
+    public boolean addTestType(TestType tt){
+        testTypeStore.listAdd(tt);
+        return true;
+    }
+
+    //public List
+
+    public boolean validateTestType(TestType tt){
+        testTypeStore.ValidateTestType(tt);
+        return true;
+    }
+
+    public boolean saveTestType(TestType tt){
+        testTypeStore.saveTestType();
+        return true;
+    }
+
+    public boolean listContainsTestType(TestType tt){
+        testTypeStore.listContain(tt);
+        return true;
+    }
+
+    public boolean listAdd(TestType tt){
+        testTypeStore.listAdd(tt);
+        return true;
+    }
+
+    public TestType getTestType(int i){
+       return testTypeStore.getTestType(i);
+    }
+
+    public TestType getTt() {
+        return testTypeStore.tt;
+    }
 
     public Company(String designation)
     {
