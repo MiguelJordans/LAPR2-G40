3b20e73124c443803671abd40e483812a437abf3
1200759_Lucas_Silva <1200759@isep.ipp.pt>
2021-05-06 16:25:46 +0100

    Little changes to US03 Description / Diagram Into Code: ClientRegistration Codification.

--- src/main/java/app/controller/CreateClientRegistrationController.java
+++ src/main/java/app/controller/CreateClientRegistrationController.java
@@ -1,6 +1,10 @@
-package auth.domain.model;
+package app.controller;
 
-public class CreateClientRegistration {
+import app.domain.model.ClientRegistration;
+
+public class CreateClientRegistrationController {
+
+    ClientRegistration cr;
 
     public void createClientRegistration(String name, long citizenCardNumber, long phoneNumber, String email, long TINNumber, long NHSNumber, String sex, String birthData){
 
