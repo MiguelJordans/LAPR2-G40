477efb5891112c80b187eaff92fdcf203d0afd4c
1200902 Mariana Lages <1200902@isep.ipp.pt>
2021-05-09 11:13:57 +0100

    US8 Javadoc implementation

--- src/main/java/app/domain/ClinicalAnalysisLaboratory.java
+++ src/main/java/app/domain/ClinicalAnalysisLaboratory.java
@@ -10,6 +10,15 @@ public class ClinicalAnalysisLaboratory {
     private String phoneNumber;
     private String tinNumber;
 
+    /**
+     * Constructs an instance of Clinical Analysis Laboratory.
+     *
+     * @param laboratoryID - the Clinical Analysis Laboratory's laboratory ID
+     * @param name         - the Clinical Analysis Laboratory's name
+     * @param address      - the Clinical Analysis Laboratory's address
+     * @param phoneNumber  - the Clinical Analysis Laboratory's phone number
+     * @param tinNumber    - the Clinical Analysis Laboratory's TIN number
+     */
     public ClinicalAnalysisLaboratory(String laboratoryID, String name, String address, String phoneNumber, String tinNumber) {
         this.laboratoryID = laboratoryID;
         this.name = name;
@@ -26,47 +35,97 @@ public class ClinicalAnalysisLaboratory {
 
     //  Gets --------------------------------------------------------------------
 
+    /**
+     * Returns the laboratory ID of the Clinical Analysis Laboratory.
+     *
+     * @return the laboratory ID of the Clinical Analysis Laboratory
+     */
     public String getLaboratoryID() {
         return laboratoryID;
     }
 
+    /**
+     * Returns the name of the Clinical Analysis Laboratory.
+     *
+     * @return the name of the Clinical Analysis Laboratory
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the address of the Clinical Analysis Laboratory.
+     *
+     * @return the address of the Clinical Analysis Laboratory
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Returns the phone number of the Clinical Analysis Laboratory.
+     *
+     * @return the phone number of the Clinical Analysis Laboratory
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Returns the TIN number of the Clinical Analysis Laboratory.
+     *
+     * @return the TIN number of the Clinical Analysis Laboratory
+     */
     public String getTINnumber() {
         return tinNumber;
     }
 
     //  Sets --------------------------------------------------------------------
 
+    /**
+     * Modifies the laboratory ID of the Clinical Analysis Laboratory.
+     *
+     * @param laboratoryID - the Clinical Analysis Laboratory's laboratory ID
+     */
     public void setLaboratoryID(String laboratoryID) {
         this.laboratoryID = laboratoryID;
     }
 
+    /**
+     * Modifies the name of the Clinical Analysis Laboratory.
+     *
+     * @param name - the Clinical Analysis Laboratory's name
+     */
     public void setName(String name) {
         checkNameRules(name);
         this.name = name;
     }
 
+    /**
+     * Modifies the address of the Clinical Analysis Laboratory.
+     *
+     * @param address - the Clinical Analysis Laboratory's address
+     */
     public void setAddress(String address) {
         checkAddressRules(address);
         this.address = address;
     }
 
+    /**
+     * Modifies the phone number of the Clinical Analysis Laboratory.
+     *
+     * @param phoneNumber - the Clinical Analysis Laboratory's phone number
+     */
     public void setPhoneNumber(String phoneNumber) {
         checkPhoneNumberRules(phoneNumber);
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * Modifies the TIN number of the Clinical Analysis Laboratory.
+     *
+     * @param tinNumber - the Clinical Analysis Laboratory's TIN number
+     */
     public void setTINnumber(String tinNumber) {
         checkTinNumberRules(tinNumber);
         this.tinNumber = tinNumber;
@@ -74,6 +133,11 @@ public class ClinicalAnalysisLaboratory {
 
     //  Checks --------------------------------------------------------------------
 
+    /**
+     * Checks the laboratory ID rules.
+     *
+     * @param laboratoryID - the Clinical Analysis Laboratory's laboratory ID
+     */
     private void checkLaboratoryIDRules(String laboratoryID) {
         if (StringUtils.isBlank(laboratoryID)) {
             throw new IllegalArgumentException("LaboratoryID cannot be blank.");
@@ -83,6 +147,11 @@ public class ClinicalAnalysisLaboratory {
         }
     }
 
+    /**
+     * Checks the name rules.
+     *
+     * @param name - the Clinical Analysis Laboratory's name
+     */
     private void checkNameRules(String name) {
         String aux = name.trim();
         aux = aux.toLowerCase();
@@ -102,6 +171,11 @@ public class ClinicalAnalysisLaboratory {
         }
     }
 
+    /**
+     * Checks the address rules.
+     *
+     * @param address - the Clinical Analysis Laboratory's address
+     */
     private void checkAddressRules(String address) {
         if (StringUtils.isBlank(address)) {
             throw new IllegalArgumentException("Address cannot be blank.");
@@ -111,6 +185,11 @@ public class ClinicalAnalysisLaboratory {
         }
     }
 
+    /**
+     * Checks the phone number rules.
+     *
+     * @param phoneNumber - the Clinical Analysis Laboratory's phone number
+     */
     private void checkPhoneNumberRules(String phoneNumber) {
         String auxTrimPhoneNumber = phoneNumber.replaceAll("\\s", "");
         char[] auxChar = auxTrimPhoneNumber.toCharArray();
@@ -123,14 +202,19 @@ public class ClinicalAnalysisLaboratory {
         }
         for (int i = 0; i < auxChar.length; i++) {
             char ch = auxChar[i];
-            if (ch >= 'a' && ch <= 'z') {
+            if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') {
                 throw new IllegalArgumentException("Invalid phone number! Try using only numbers.");
             }
         }
     }
 
+    /**
+     * Checks the TIN number rules.
+     *
+     * @param tinNumber - the Clinical Analysis Laboratory's TIN number
+     */
     private void checkTinNumberRules(String tinNumber) {
-        String auxTrimTINnumber = tinNumber.replaceAll("\\p{Z}", "");
+        String auxTrimTINnumber = tinNumber.replaceAll("\\s", "");
         char[] auxChar = auxTrimTINnumber.toCharArray();
 
         if (StringUtils.isBlank(tinNumber)) {
@@ -141,12 +225,18 @@ public class ClinicalAnalysisLaboratory {
         }
         for (int i = 0; i < auxChar.length; i++) {
             char ch = auxChar[i];
-            if (ch >= 'a' && ch <= 'z') {
+            if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z') {
                 throw new IllegalArgumentException("Invalid TIN number! Try using only numbers.");
             }
         }
     }
 
+    /**
+     * Returns the textual description of the Clinical Analysis Laboratory in the format: laboratory ID, name,
+     * address, phone number, TIN number.
+     *
+     * @return
+     */
     @Override
     public String toString() {
         return "LaboratoryID: " + laboratoryID + ", Name: " + name + ", Address: " + address + ", Phone number: " + phoneNumber + ", TIN number: " + tinNumber;
