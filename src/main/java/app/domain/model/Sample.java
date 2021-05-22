f2cdb65d4347895ea549ac0dcd4e3c6ca355487b
MiguelJordao <1201487@isep.ipp.pt>
2021-05-22 20:44:47 +0100

    Design to code (PPROG)

---
+++ src/main/java/app/domain/model/Sample.java
@@ -0,0 +1,51 @@
+/*package app.domain.model;
+
+import net.sourceforge.barbecue.Barcode;
+import net.sourceforge.barbecue.BarcodeException;
+import net.sourceforge.barbecue.BarcodeFactory;
+import net.sourceforge.barbecue.BarcodeImageHandler;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Sample {
+
+    private final double MIN = 100000000000l;
+    private final double MAX = 999999999999l;
+
+    private TestTypeStore ts;
+    private Barcode barcode;
+
+    public Sample(TestTypeStore ts){
+
+        String barcodeText = Double.toString(generateNumber());
+
+        this.ts=ts;
+
+        try {
+            this.barcode = generateEAN13Barcode(barcodeText);
+            generateEAN13BarcodeImage(this.barcode);
+        }
+        catch (IllegalAccessException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public Barcode generateEAN13Barcode(String barcodeText) throws Exception  {
+
+         Barcode barcode = BarcodeFactory.createEAN13(barcodeText);
+
+         return barcode;
+    }
+
+    public BufferedImage generateEAN13BarcodeImage(Barcode barcode) throws Exception {
+        return BarcodeImageHandler.getImage(barcode);
+    }
+
+    public double generateNumber(){
+        double random_bar = Math.floor(Math.random()*(MAX-MIN+1)+MIN);
+        return random_bar;
+    }
+
+}  */
