158d10e744a992dc148bebfbdd243f43cf6bf800
1200759_Lucas_Silva <1200759@isep.ipp.pt>
2021-05-27 11:36:23 +0100

    Few Changes to the TestUI / Testing Classes created for Test and TestStore

--- src/test/java/app/domain/model/TestTest.java
+++ src/test/java/app/domain/model/TestTest.java
@@ -1,4 +1,23 @@
+package app.domain.model;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class TestTest {
-  
+
+    @Test
+    public void setState() {
+
+
+
+    }
+
+    @Test
+    public void createTestParameter() {
+    }
+
+    @Test
+    public void addTestParameterResult() {
+    }
 }
